36097e270128ec18d79d641e3491f5415ad85fb5
Dhaval Kapil <me@dhavalkapil.com>
2016-06-08 00:40:21 +0530

    Testing (#5)
    * Added more tests
    
    * Added luarocks install test
    
    * Added debug statements
    
    * Fixed path

--- test.sh
+++
@@ -1,4 +0,0 @@
-. ~/.luavm/luavm
-
-# Installing lua 5.3.2
-echo "Y" | luavm install 5.3.2
---
+++ tests/test.sh
@@ -0,0 +1,42 @@
+. ~/.luavm/luavm
+
+echo "Y" > yes
+
+# Installing lua 5.3.2
+luavm install 5.3.2 < yes
+
+# Confirming
+version_string=$(lua -v)
+if test "${version_string#*5.3.2}" == "${version_string}"
+then
+    exit 1
+fi
+
+# Installing lua 5.3.1
+luavm install 5.3.1 < yes
+
+# Confirming
+version_string=$(lua -v)
+if test "${version_string#*5.3.1}" == "${version_string}"
+then
+    exit 1
+fi
+
+# Installing luarocks 2.3.0
+luavm install-luarocks 2.3.0 < yes
+
+# Confirming
+version_string=$(luarocks)
+if test "${version_string#*2.3.0}" == "${version_string}"
+then
+    exit 1
+fi
+
+luarocks install --server=http://luarocks.org/dev elasticsearch
+
+lua ./tests/luarocks_test.lua
+
+if [ $? == 1 ]
+then
+	exit 1
+fi
