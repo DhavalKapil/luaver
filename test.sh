d32763453ffedf6a0617bc7299d7a3eb2452a3ab
Dhaval Kapil <me@dhavalkapil.com>
2016-06-07 14:12:51 +0000

    Added more tests

--- test.sh
+++ test.sh
@@ -1,4 +1,33 @@
 . ~/.luavm/luavm
 
+echo "Y" > yes
+
 # Installing lua 5.3.2
-echo "Y" | luavm install 5.3.2
+luavm install 5.3.2 < yes
+
+# Confirming
+version_string=$(lua -v)
+if test "${version_string#*5.3.2}" == "${version_string}"
+then
+    exit 1
+fi
+
+# Installing lua 5.3.1
+luavm install 5.3.1 < yes
+
+# Confirming
+version_string=$(lua -v)
+if test "${version_string#*5.3.1}" == "${version_string}"
+then
+    exit 1
+fi
+
+# Installing luarocks 2.3.0
+luavm install-luarocks 2.3.0 < yes
+
+# Confirming
+version_string=$(luarocks)
+if test "${version_string#*2.3.0}" == "${version_string}"
+then
+    exit 1
+fi
