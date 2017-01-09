45b79817aea705810dc6e9697c1f7ad5ef7ebdcf
Dhaval Kapil <me@dhavalkapil.com>
2017-01-09 23:02:16 +0530

    Fixed == bug in tests, shifted to =

--- tests/integration tests/default_test.sh
+++ tests/integration tests/default_test.sh
@@ -2,14 +2,14 @@
 
 # Confirming default lua version
 version_string=$(lua -v)
-if test "${version_string#*5.2.4}" == "${version_string}"
+if test "${version_string#*5.2.4}" = "${version_string}"
 then
     exit 1
 fi
 
 # Confirming
 version_string=$(luarocks)
-if test "${version_string#*2.3.0}" == "${version_string}"
+if test "${version_string#*2.3.0}" = "${version_string}"
 then
     exit 1
 fi
